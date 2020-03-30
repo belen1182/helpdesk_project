a6737ddc8725916a682dee464c0b3b30d85b6b78
belen <belenloal@gmail.com>
2020-03-30 20:38:26 +0200

    Codigo helpdesk_project subido para probar el boton New Ticket de Tareas que no funciona

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Helpdesk Project",
+    "summary": """
+        Helpdesk""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "After-Sales",
+    "author": "AdaptiveCity, "
+    "C2i Change 2 Improve, "
+    "Domatix, "
+    "Factor Libre, "
+    "SDi Soluciones, "
+    "PESOL ,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/helpdesk",
+    "depends": [
+        "helpdesk_mgmt",
+        "project"
+    ],
+    "data": [
+        "views/project_views.xml",
+        "views/ticket_views.xml"
+    ],
+    "demo": [],
+    "development_status": "Alpha",
+    "application": True,
+    "installable": True,
+}
